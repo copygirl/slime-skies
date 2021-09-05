b19441b249111c45317f7e3ed0e505eba7383769
copygirl <copygirl@mcft.net>
2021-09-05 18:55:53 +0200

    Disable dirt bucket recipes

--- kubejs/server_scripts/recipe_tweaks.js
+++ kubejs/server_scripts/recipe_tweaks.js
@@ -3,5 +3,6 @@
 onEvent("recipes", event => {
   event.remove({ id: "minecraft:bread" });
   event.remove({ id: "quark:tweaks/crafting/utility/bent/bread" });
+  event.remove({ id: "dynamictrees:dirt_bucket" });
   event.remove({ id: "create:milling/andesite" });
 });
