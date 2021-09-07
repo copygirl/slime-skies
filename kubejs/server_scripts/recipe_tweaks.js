0aef57192fd02773a4a4ffd8d1a6b87cfecd52ea
copygirl <copygirl@mcft.net>
2021-09-07 10:24:20 +0200

    Change cake and cookie recipe to use dough

--- kubejs/server_scripts/recipe_tweaks.js
+++ kubejs/server_scripts/recipe_tweaks.js
@@ -3,6 +3,7 @@
 onEvent("recipes", event => {
   event.remove({ id: "minecraft:bread" });
   event.remove({ id: "quark:tweaks/crafting/utility/bent/bread" });
+  event.remove({ id: "quark:tweaks/crafting/utility/bent/cookie" });
   event.remove({ id: "dynamictrees:dirt_bucket" });
   event.remove({ id: "create:milling/andesite" });
 });
