8058634f72b79021542a2ad1563877b3c8a262c5
copygirl <copygirl@mcft.net>
2021-09-29 19:23:28 +0200

    Add apple pip recipe

--- kubejs/server_scripts/recipe_tweaks.js
+++ kubejs/server_scripts/recipe_tweaks.js
@@ -4,8 +4,8 @@ onEvent("recipes", event => {
   event.remove({ id: "minecraft:bread" });
   event.remove({ id: "quark:tweaks/crafting/utility/bent/bread" });
   event.remove({ id: "quark:tweaks/crafting/utility/bent/cookie" });
-  event.remove({ id: "dynamictrees:dirt_bucket" });
   event.remove({ id: "create:milling/andesite" });
   event.remove({ id: "cavesandcliffs:copper_ingot_from_smelting_deepslate_copper_ore" });
   event.remove({ id: "cavesandcliffs:copper_ingot_from_blasting_deepslate_copper_ore" });
+  event.remove({ mod: "dynamictrees" });
 });
